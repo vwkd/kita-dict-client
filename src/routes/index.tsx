872b1d548ba00439b1ca0a577cae02f917df6119
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-05 14:01:22 +0200

    feat: add sublines

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -31,13 +31,32 @@ export default function Page({ data }) {
   return (
     <body class="px-5 py-3 flex flex-col gap-5 items-center">
       <h1 class="text-3xl font-semibold">Kita Dict</h1>
-      <ul class="flex flex-col gap-3 max-w-screen-sm">
+      <ul class="flex flex-col max-w-screen-sm">
       { entries.map((line) => {
-        // only to make `**` bold and `*` cursive
-        const html = gfm.render(line);
-        return (
-            <li dangerouslySetInnerHTML={{ __html: html }} class="flex gap-3"></li>
-          );
+          if (line.includes("\n  ")) {
+            const sublines = line.split("\n  ");
+          
+            return (
+              <li class="py-4 border-b-2 last:border-0 flex flex-col">
+                <ul class="flex flex-col">
+                  { sublines.map((subline, i) => {
+                    // only to make `**` bold and `*` cursive
+                    const html = gfm.render(subline);
+                    return (
+                      <li dangerouslySetInnerHTML={{ __html: html }} class={`${i > 0 ? "ml-2" : ""} ${i > 1 ? "mt-2" : ""}`}></li>
+                      );
+                    })
+                  }
+                </ul>
+              </li>
+            );
+          } else {
+            // only to make `**` bold and `*` cursive
+            const html = gfm.render(line);
+            return (
+              <li dangerouslySetInnerHTML={{ __html: html }} class="py-4 border-b-2 last:border-0" style="text-indent: -0.5rem; padding-left: 0.5rem;"></li>
+            );
+          }
         })
       }
       </ul>
