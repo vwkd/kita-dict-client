735fa1ead25d74dddea9e835d3364880a28188c5
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-06 03:02:57 +0200

    feat: rework, add status, use /results route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,65 +1,42 @@
-import * as gfm from "https://deno.land/x/gfm@0.1.23/mod.ts";
-
-const SERVER_URL = "https://vwkd-kita-dict-server.deno.dev/";
+const SERVER_URL = "http://vwkd-kita-dict-server.deno.dev/status";
 
 export const handler = {
   async GET(req, ctx) {
     const url = new URL(req.url);
     const q = url.searchParams.get("q");
     
-    if (q === null) {
-      return new Response("No query provided");
-    } else {
-      const urlServer = new URL(SERVER_URL);
-      urlServer.searchParams.set("q", q);
-
-      const res = await fetch(urlServer);
-      const entries = await res.json();
-
-      if (!entries.length) {
-        entries.push("No results found.");
-      }
-  
-      return ctx.render({ entries });
+    // note: deprecated legacy queries on root
+    if (q !== null) {
+      return new Response("Use /results endpoint", {
+        status: 301,
+        headers: { location: `/results${url.search}` },
+      });
     }
+
+    const res = await fetch(SERVER_URL);
+    const status = await res.json();
+
+    return ctx.render({ status });
   },
 };
 
 export default function Page({ data }) {
-  const entries = data.entries;
-  
+  const { book, last_page } = data.status;
+
   return (
     <body class="px-5 py-3 flex flex-col gap-5 items-center">
       <h1 class="text-3xl font-semibold">Kita Dict</h1>
-      <ul class="flex flex-col max-w-screen-sm">
-      { entries.map((line) => {
-          if (line.includes("\n  ")) {
-            const sublines = line.split("\n  ");
-          
-            return (
-              <li class="py-4 border-b-2 last:border-0 flex flex-col">
-                <ul class="flex flex-col">
-                  { sublines.map((subline, i) => {
-                    // only to make `**` bold and `*` cursive
-                    const html = gfm.render(subline);
-                    return (
-                      <li dangerouslySetInnerHTML={{ __html: html }} class={`${i > 0 ? "ml-2" : ""} ${i > 1 ? "mt-2" : ""}`}></li>
-                      );
-                    })
-                  }
-                </ul>
-              </li>
-            );
-          } else {
-            // only to make `**` bold and `*` cursive
-            const html = gfm.render(line);
-            return (
-              <li dangerouslySetInnerHTML={{ __html: html }} class="py-4 border-b-2 last:border-0" style="text-indent: -0.5rem; padding-left: 0.5rem;"></li>
-            );
-          }
-        })
-      }
-      </ul>
+      <div class="flex flex-col">
+        <h2 class="text-lg font-semibold">Progress Status</h2>
+        <div class="flex gap-2">
+          <h3>Book</h3>
+          <p>{book}</p>
+        </div>
+        <div class="flex gap-2">
+          <h3>Page</h3>
+          <p>{last_page}</p>
+        </div>
+      </div>
     </body>
   );
 }
---
+++ src/routes/results/index.tsx
@@ -0,0 +1,70 @@
+import * as gfm from "gfm/mod.ts";
+
+const SERVER_URL = "http://vwkd-kita-dict-server.deno.dev/results";
+
+export const handler = {
+  async GET(req, ctx) {
+    const url = new URL(req.url);
+    const q = url.searchParams.get("q");
+    
+    if (q === null) {
+      return new Response("Missing query parameter", { status: 400 });
+    }
+    
+    const urlServer = new URL(SERVER_URL);
+    urlServer.searchParams.set("q", q);
+
+    const res = await fetch(urlServer);
+    const results = await res.json();
+
+    return ctx.render({ results });
+  },
+};
+
+export default function Page({ data }) {
+  const results = data.results;
+  
+  if (!results.length) {
+    return (
+      <body class="px-5 py-3 flex flex-col gap-5 items-center">
+        <h1 class="text-3xl font-semibold">Kita Dict</h1>
+        <p>No results found.</p>
+      </body>
+    );
+  }
+
+  return (
+    <body class="px-5 py-3 flex flex-col gap-5 items-center">
+      <h1 class="text-3xl font-semibold">Kita Dict</h1>
+      <ul class="flex flex-col max-w-screen-sm">
+      { results.map((line) => {
+          if (line.includes("\n  ")) {
+            const sublines = line.split("\n  ");
+          
+            return (
+              <li class="py-4 border-b-2 last:border-0 flex flex-col">
+                <ul class="flex flex-col">
+                  { sublines.map((subline, i) => {
+                    // only to make `**` bold and `*` cursive
+                    const html = gfm.render(subline);
+                    return (
+                      <li dangerouslySetInnerHTML={{ __html: html }} class={`${i > 0 ? "ml-2" : ""} ${i > 1 ? "mt-2" : ""}`}></li>
+                      );
+                    })
+                  }
+                </ul>
+              </li>
+            );
+          } else {
+            // only to make `**` bold and `*` cursive
+            const html = gfm.render(line);
+            return (
+              <li dangerouslySetInnerHTML={{ __html: html }} class="py-4 border-b-2 last:border-0" style="text-indent: -0.5rem; padding-left: 0.5rem;"></li>
+            );
+          }
+        })
+      }
+      </ul>
+    </body>
+  );
+}
