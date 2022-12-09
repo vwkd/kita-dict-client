c17b52b9dcb776154e20c9c4f70e62e4ffafcc8c
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-09 03:42:05 +0200

    fix: patch illegal bold

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,15 +4,22 @@ import { render } from "gfm/mod.ts";
 const ESCAPE_CHARS = /([\[\]\<\>])/g;
 const ESCAPE_TILDE = /(?<!\~)(\~)(?!\~)/g;
 
-// only to make `**` bold and `*` cursive
+const ILLEGAL_BOLD = /(\))(\*\*)(?! |$)/g;
+const PLACEHOLDER_BOLD = /(\))X(<\/strong>)/g;
+
+// only makes `**` bold and `*` cursive
 // escapes other markdown characters present in dict
 // beware: escape single tilde since some are converted into `<del>`, e.g. `გა~, და~, მი~` becomes `გა<del>, და</del>, მი~`
+// patches illegal bold ending in symbol and not followed by space, e.g. `**ბნე(ვ)**¹`, `ა**ფუ(ვ)**ებული`
+// adds filler letter `X` in markdown and removes again in html
 export function renderMarkdown(str: string) {
   const strEscaped = str
     .replace(ESCAPE_CHARS, "\\$1")
-    .replace(ESCAPE_TILDE, "\\$1");
-
-  const html = render(strEscaped);
+    .replace(ESCAPE_TILDE, "\\$1")
+    .replace(ILLEGAL_BOLD, "$1X$2");
+  
+  const html = render(strEscaped)
+    .replace(PLACEHOLDER_BOLD, "$1$2");
   
   return html;
 }
