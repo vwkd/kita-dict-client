5339ef59928797e1529ffe7b84bc9db7395956fc
vwkd <33468089+vwkd@users.noreply.github.com>
2022-12-16 00:04:53 +0200

    feat: add number of entries

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,7 @@ import type { Handlers, PageProps } from "$fresh/server.ts";
 const SERVER_URL = "http://vwkd-kita-dict-server.deno.dev/status";
 
 type Status = {
+  entries: number;
   progress: number;
   pages: number;
   pagesTotal: number;
@@ -30,14 +31,22 @@ export const handler: Handlers = {
 };
 
 export default function Page({ data }: PageProps<{ status: Status }>) {
-  const { progress, pages, pagesTotal } = data.status;
+  const { entries, progress, pages, pagesTotal } = data.status;
 
   return (
     <Layout>
       <h1 class="text-3xl font-semibold">Kita Dict</h1>
-      <div class="flex flex-col items-center">
-        <h2 class="text-lg font-semibold">Progress Status</h2>
-        <p>{`${progress}% (${pages}/${pagesTotal} pages)`}</p>
+      <div class="flex flex-col items-center gap-3">
+        <h2 class="text-2xl font-semibold">Status</h2>
+        <div class="flex flex-col items-center">
+          <h3 class="text-lg font-semibold">Entries</h3>
+          <p>{entries}</p>
+        </div>
+        <div class="flex flex-col items-center">
+          <h3 class="text-lg font-semibold">Progress</h3>
+          <p>{`${progress}%`}</p>
+          <p>{`(${pages}/${pagesTotal} pages)`}</p>
+        </div>
       </div>
     </Layout>
   );
