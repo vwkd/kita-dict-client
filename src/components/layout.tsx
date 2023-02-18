3c8a69e07754c3f530e27075af6b831d0e2b4ad4
vwkd <33468089+vwkd@users.noreply.github.com>
2023-02-18 22:20:57 +0200

    fix: search field, wrap nav on mobile

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,10 +11,10 @@ export default function Page(
 
   return (
     <body class="px-5 py-3 max-w-screen-sm mx-auto flex flex-col gap-5 items-center bg-white dark:bg-black text-gray-800 dark:text-gray-300">
-      <nav class="flex gap-5 items-center">
+      <nav class="self-stretch flex-1 flex flex-wrap gap-5 items-center justify-center">
         <NavButton href="/" title="Home" />
-        <SearchBox query={query} pathname={pathname} />
         <NavButton href="/reference" title="Reference" />
+        <SearchBox query={query} pathname={pathname} />
       </nav>
       {children}
     </body>
--- src/components/searchbox.tsx
+++ src/components/searchbox.tsx
@@ -4,7 +4,7 @@ export default function SearchBox(
   return (
     <form
       action={pathname != "/results" ? "/results" : ""}
-      class="flex-1 gap-3"
+      class="flex-1 flex gap-3"
     >
       <label for="q" class="sr-only">Search</label>
       <input
@@ -13,7 +13,7 @@ export default function SearchBox(
         value={query ?? ""}
         placeholder="Search..."
         required
-        class="px-3 py-2 dark:bg-gray-800 border dark:border-none border-slate-300 rounded-md placeholder-slate-400 dark:placeholder-gray-600 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+        class="flex-1 px-3 py-2 dark:bg-gray-800 border dark:border-none border-slate-300 rounded-md placeholder-slate-400 dark:placeholder-gray-600 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
       />
     </form>
   );
