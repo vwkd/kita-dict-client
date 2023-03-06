3005e22ad61cb88cdcdc10c6ecdfedc6076322ae
vwkd <33468089+vwkd@users.noreply.github.com>
2023-03-07 00:07:35 +0200

    feat: proper landing page description

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -24,19 +24,39 @@ export default function Page({ data }: PageProps<{ status: Status }>) {
 
   return (
     <Layout>
-      <h1 class="text-3xl font-semibold">Kita Dict</h1>
-      <div class="flex flex-col items-center gap-3">
-        <h2 class="text-2xl font-semibold">Status</h2>
-        <div class="flex flex-col items-center">
-          <h3 class="text-lg font-semibold">Entries</h3>
-          <p>{entries}</p>
-        </div>
-        <div class="flex flex-col items-center">
-          <h3 class="text-lg font-semibold">Progress</h3>
-          <p>{`${progress}%`}</p>
-          <p>{`(${pages}/${pagesTotal} pages)`}</p>
-        </div>
-      </div>
+      <main class="flex-1 flex flex-col gap-10">
+        <header class="flex flex-col gap-3">
+          <h1 class="text-5xl font-semibold text-slate-500 dark:text-gray-600">
+            Kita Dict
+          </h1>
+        </header>
+        <section class="flex flex-col gap-3">
+          <p>
+            A Georgian German dictionary with over 60.000 entries.
+          </p>
+          <p>
+            It is a work in progress - see status below.
+          </p>
+        </section>
+        <section class="flex flex-col gap-3">
+          <h2 class="text-2xl font-semibold text-slate-500 dark:text-gray-600">
+            Status
+          </h2>
+          <div class="flex flex-col">
+            <h3 class="text-lg font-semibold text-slate-500 dark:text-gray-600">
+              Entries
+            </h3>
+            <p>{entries}</p>
+          </div>
+          <div class="flex flex-col">
+            <h3 class="text-lg font-semibold text-slate-500 dark:text-gray-600">
+              Progress
+            </h3>
+            <p>{`${progress}%`}</p>
+            <p>{`(${pages}/${pagesTotal} pages)`}</p>
+          </div>
+        </section>
+      </main>
     </Layout>
   );
 }
