682da31ef2f378d9b9a32bb889bd2c51b26d0514
vwkd <33468089+vwkd@users.noreply.github.com>
2023-03-07 18:41:16 +0200

    fix: minimum size of search box

--- src/components/searchbox.tsx
+++ src/components/searchbox.tsx
@@ -13,6 +13,7 @@ export default function SearchBox(
         value={query ?? ""}
         placeholder="Search..."
         required
+        size="1"
         class="flex-1 px-3 py-2 dark:bg-gray-800 border dark:border-none border-slate-300 rounded-md placeholder-slate-400 dark:placeholder-gray-600 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
       />
     </form>
